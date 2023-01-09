fd706dc96861e75f820dad7f9c39587cd0a84bdf
Vadym Olkhovyk <vadymolhovyk@gmail.com>
2023-01-09 18:58:36 +0200

    Added Post image

--- learnsite/blog/models.py
+++ learnsite/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from PIL import Image
 
 
 class Post(models.Model):
@@ -7,5 +8,17 @@ class Post(models.Model):
     text = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
     post_slug = models.CharField(max_length=80, default="default_post")
+    img = models.ImageField(default = 'blog/static/img/default.png',
+                            upload_to='blog/static/img',
+                            height_field=None,
+                            width_field=None,
+                            max_length=200,
+                            verbose_name="Картинка для поста")
+    def save(self, *agr, **kwargs):
+        super().save()
+        img = Image.open(self.img.path)
+        if img.height>720 or img.width>1080:
+            img.thumbnail((720, 1080))
+            img.save(self.img.path)
     def __str__(self):
         return self.title
--- learnsite/learnsite/settings.py
+++ learnsite/learnsite/settings.py
@@ -116,7 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'blog/static/img/'
+STATICFILES_DIRS = [
+    BASE_DIR / "blog/static",
+    BASE_DIR / "blog/static/img",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
